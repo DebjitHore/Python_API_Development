6059cf94ca7afef4646b58ff1c9e91f289b1e1dc
DebjitHore <debjithore@gmail.com>
2022-08-20 22:35:21 +0530

    implemented all basic functionalities

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,5 +1,6 @@
+from asyncio.proactor_events import _ProactorDuplexPipeTransport
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -31,7 +32,7 @@ async def get_posts():
 #     return {"new_post": f"title {payload['title']} content {payload['content']}"}
 #title string and content string is mandatory in the data schema
 
-@app.post("/newposts")
+@app.post("/newposts", status_code=status.HTTP_201_CREATED )
 async def create_posts(newPost: Post): #referencing the post pydantic model, does the data validation and also verifies the schema of the data being sent from the front-end
     #print (newPost.rating)
     #print(newPost.dict()) #converting the pydantic object into a python dictionary
@@ -41,12 +42,42 @@ async def create_posts(newPost: Post): #referencing the post pydantic model, doe
     return {"data" : post_dict}
 
 def find_post(id):
-    for p in my_posts:
+    for i, p in enumerate(my_posts):
         if p['id']== id:
-            return p
+            return (i,p)
+        else:
+            return (None, None)
 
 @app.get("/posts/{id}")
-async def get_post(id: int):
-    post= find_post(id)
+async def get_post(id: int, response: Response):
+    index,post= find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Post with id : {id} not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'message': f"Post with id : {id} not found"}
     return {"post detail": post}
-    
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT )
+async def delete_post(id :int):
+    index,post= find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Post with id : {id} not found and can not be deleted")
+    else:
+        my_posts.pop(index )
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+async def update_post(id : int, post: Post):
+    index,postSingle= find_post(id)
+    if not postSingle:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Post with id : {id} not found and can not be deleted")
+    post_dict= post.dict()
+    post_dict['id']= id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
+
